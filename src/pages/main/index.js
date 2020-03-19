acd316e1a6a8d8ff0323c4665da906eb46cb97d8
Lucas Bastos <lucasbastos@gmail.com>
2020-03-18 22:34:40 -0300

    adicionando butões de paginação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,18 +4,36 @@ import api from "../../services/api";
 
 export default class Main extends Component {
     state = {
-        products: []
+        products: [],
+        product_info: {},
+        page: 1
     };
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get("/products");
-        this.setState({ products: response.data.docs });
+    nextPage = () => {
+        const { page, product_info } = this.state;
+        console.log(page);
+        if (page === product_info.pages) return;
+        this.setState({ page: page + 1 });
+        this.loadProducts(page + 1);
+    };
+    previousPage = () => {
+        const { page } = this.state;
+        if (page === 1) return;
+        this.setState({ page: page - 1 });
+        this.loadProducts(page - 1);
+    };
+
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+        const { docs, ...product_info } = response.data;
+        this.setState({ products: docs, product_info, page });
     };
     render() {
-        const { products } = this.state;
+        const { products, page, product_info } = this.state;
+        console.log("aa", page, product_info);
         return (
             <div className="product-list">
                 {products.map(product => {
@@ -28,8 +46,15 @@ export default class Main extends Component {
                     );
                 })}
                 <div className="actions">
-                    <button>Anterior</button>
-                    <button>Proximo</button>
+                    <button disabled={page === 1} onClick={this.previousPage}>
+                        Anterior
+                    </button>
+                    <button
+                        disabled={page === product_info.pages}
+                        onClick={this.nextPage}
+                    >
+                        Proximo
+                    </button>
                 </div>
             </div>
         );
